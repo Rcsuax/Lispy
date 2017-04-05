e86555e6d694645faf9266f5c036a00450597841
rcsuax <rcsuax@gmail.com>
2017-04-05 11:06:59 +0100

    installs dependencies if they are not present

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 echo "Compiling Lispy"
+
+LIBEDIT=$(dpkg -s libedit-dev | grep Status | awk -F 'install' '{print $2}')
+
+if [ $LIBEDIT != "ok" ]; then
+		sudo apt-get install libedit-dev
+fi
+
 cc -std=c99 -Wall $PWD/parsing.c $PWD/mpc.c -ledit -lm -o $PWD/parsing
